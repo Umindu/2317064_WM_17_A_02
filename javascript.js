f4cc7374d259e356d4da54af6158166d5c73a1c8
Umindu <ybumindu@gmail.com>
2023-10-15 18:51:02 +0530

    2

--- javascript.js
+++ javascript.js
@@ -12,10 +12,17 @@
 
 
 function PlayBtnClick(player) {
-    var elements = document.getElementsByClassName("face");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.add("anim");
+    console.log("###");
+
+    var elements1 = document.getElementsByClassName("container-one");
+    var elements2 = document.getElementsByClassName("container-two");
+
+
+    for (var i = 0; i < elements1.length; i++) {
+        elements1[i].classList.add("anim");
     }
 
-    console.log(player)
+    for (var i = 0; i < elements2.length; i++) {
+        elements2[i].classList.add("anim4");
+    }
 }
