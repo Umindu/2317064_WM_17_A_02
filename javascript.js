785f1f18f20f3304174718ba369c6932aad7a693
Umindu <ybumindu@gmail.com>
2023-10-16 13:39:59 +0530

    3

--- javascript.js
+++ javascript.js
@@ -10,19 +10,101 @@
 //     });
 // });
 
+var p1TotalScore = 0;
+var p2TotalScore = 0;
 
-function PlayBtnClick(player) {
-    console.log("###");
+document.getElementById("secondButton").disabled = true;
 
-    var elements1 = document.getElementsByClassName("container-one");
-    var elements2 = document.getElementsByClassName("container-two");
+async function PlayBtnClick(player) {
+    var audio = new Audio('image/sound.mp3');
+    audio.play();
+    
 
+    var ran1 = Math.floor(Math.random() * 6) + 1;
+    var ran2 = Math.floor(Math.random() * 6) + 1;
 
-    for (var i = 0; i < elements1.length; i++) {
-        elements1[i].classList.add("anim");
+    var elements1 = document.getElementById("containerOne");
+    var elements2 = document.getElementById("containerTwo");
+
+    var cube1 = document.getElementById("cubeOne");
+    var cube2 = document.getElementById("cubeTwo");
+
+    if(player === 'one'){
+        document.getElementById("fristButton").disabled = true;
+        document.getElementById("secondButton").disabled = true;
+    }
+    else{
+        document.getElementById("secondButton").disabled = true;
+        document.getElementById("fristButton").disabled = true;
     }
 
-    for (var i = 0; i < elements2.length; i++) {
-        elements2[i].classList.add("anim4");
-    }
+    elements1.classList.add("anim");
+    elements2.classList.add("anim4");
+
+    cube1.classList.add("anim2");
+    cube2.classList.add("anim3");
+
+    setTimeout(function () {
+
+        elements1.style.animationPlayState = 'paused';
+        elements2.style.animationPlayState = 'paused';
+
+        cube1.classList.remove("anim2");
+        cube2.classList.remove("anim3");
+
+        switch (ran1) {
+            case 1:
+                cube1.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                break;
+            case 2:
+                cube1.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                break;
+            case 3:
+                cube1.style.transform = 'rotateX(270deg) rotateY(0deg)';
+                break;
+            case 4:
+                cube1.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                break;
+            case 5:
+                cube1.style.transform = 'rotateX(180deg) rotateY(90deg)';
+                break;
+            case 6:
+                cube1.style.transform = 'rotateX(180deg) rotateY(270deg)';
+                break;
+        }
+
+        switch (ran2) {
+            case 1:
+                cube2.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                break;
+            case 2:
+                cube2.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                break;
+            case 3:
+                cube2.style.transform = 'rotateX(270deg) rotateY(0deg)';
+                break;
+            case 4:
+                cube2.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                break;
+            case 5:
+                cube2.style.transform = 'rotateX(180deg) rotateY(90deg)';
+                break;
+            case 6:
+                cube2.style.transform = 'rotateX(180deg) rotateY(270deg)';
+                break;
+        }
+
+        if(player === 'one'){
+            p1TotalScore = p1TotalScore+ran1+ran2
+            document.getElementById("scoreOne").innerHTML = "Score : "+(p1TotalScore);
+            document.getElementById("secondButton").disabled = false;
+        }
+        else{
+            p2TotalScore = p2TotalScore+ran1+ran2
+            document.getElementById("scoreTwo").innerHTML = "Score : "+(p2TotalScore);
+            document.getElementById("fristButton").disabled = false;
+        }
+
+    }, 970);
+
 }
